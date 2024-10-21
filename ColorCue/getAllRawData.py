fixed the computation of velo on the raw data

ColorCue/getAllRawData.py
@@ -734,11 +734,14 @@ def getAllRawData(data_dir,sampling_freq=1000, degToPix=27.28):
                     )
                     for t in df.trial.unique()
                 ]
-                velo = [
-                    pd.Series(np.gradient(df[df["trial"] == t]["xp"])*sampling_freq/degToPix for t in df.trial.unique())
-                ]
+                velo =  [
+                    np.gradient(df[df["trial"] == t]["xp"])*sampling_freq/degToPix  for t in df.trial.unique()
+                    ]
+                # Concatenate the list of arrays into a single array
                 allFiltData = pd.concat(filtered_data, axis=0, ignore_index=True)
-                allVelo = pd.concat(velo, axis=0, ignore_index=True)
+                allVelo =np.concatenate(velo)
+                print(len(allVelo))
+                
                 # Ensure the original DataFrame and the filtered DataFrame have the same length
                 if len(df) == len(allFiltData):
                     df[["filtPos", "filtVelo"]] = allFiltData