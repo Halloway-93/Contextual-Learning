0db8956264c4c0dab0252d9bdae26abad25e5f72
Halloway-93 <hamzaelhallaoui@gmail.com>
2024-10-30 18:33:08 +0100

    added the paths to pwAnalyis scripts

--- joblibPwAnalysis.py
+++ joblibPwAnalysis.py
@@ -1,6 +1,8 @@
-from joblib import Parallel, delayed
 import piecewise_regression as pw
 import numpy as np
+import json
+import pandas as pd
+import os
 
 
 def interpolateData(data):
@@ -18,6 +20,7 @@ def interpolateData(data):
         )
         return interpolated_data
     return None
+
 
 def fit_trial(df_sub_p, trial, slopes, breakpoints):
     x = df_sub_p[df_sub_p["trial"] == trial].time.values
@@ -42,17 +45,24 @@ def fit_trial(df_sub_p, trial, slopes, breakpoints):
             bps.append(bp)
     return (alphas, bps)
 
+
 def fit_condition(df_sub, proba, slopes, breakpoints):
     df_sub_p = df_sub[df_sub["proba"] == proba]
     trials = df_sub_p.trial.unique()
-    allFitTrials = {trial: fit_trial(df_sub_p, trial, slopes, breakpoints) for trial in trials}
+    allFitTrials = {
+        trial: fit_trial(df_sub_p, trial, slopes, breakpoints) for trial in trials
+    }
     return allFitTrials
+
 
 def fit_subject(df, sub, slopes, breakpoints):
     df_sub = df[df["sub"] == sub]
     probas = df_sub.proba.unique()
-    allFitConditions = {proba: fit_condition(df_sub, proba, slopes, breakpoints) for proba in probas}
+    allFitConditions = {
+        proba: fit_condition(df_sub, proba, slopes, breakpoints) for proba in probas
+    }
     return allFitConditions
+
 
 def pwAnalysis(df):
     maxBreakPoints = 3
@@ -62,7 +72,54 @@ def pwAnalysis(df):
     allFit = {sub: fit_subject(df, sub, slopes, breakpoints) for sub in subjects}
     return allFit
 
+
+def save_fit_data(fit_data, filename):
+    with open(filename, "w") as f:
+        json.dump(fit_data, f)
+
+
+dirPath1 = "/envau/work/brainets/oueld.h/contextuaLearning/ColorCue/data/"
+filteredRawData1 = "JobLibProcessingCC.csv"
+allEventsFile1 = "allEvents.csv"
+
+dirPath2 = "/envau/work/brainets/oueld.h/contextuaLearning/directionCue/results_voluntaryDirection"
+filteredRawData2 = "JobLibProcessing.csv"
+allEventsFile2 = "allEvents.csv"
+
+
+dirPath3 = "/envau/work/brainets/oueld.h/contextuaLearning/ColorCue/imposedColorData"
+filteredRawData3 = "JobLibProcessingCC.csv"
+allEventsFile3 = "allEvents.csv"
+
+dirPath4 = "/envau/work/brainets/oueld.h/contextuaLearning/directionCue/results_imposeDirection"
+filteredRawData4 = "JobLibProcessing.csv"
+allEventsFile4 = "allEvents.csv"
+
+dirPath5 = "/envau/work/brainets/oueld.h/attentionalTask/data"
+filteredRawData5 = "JobLibProcessingCC.csv"
+allEventsFile5 = "allEvents.csv"
+
+paths = [dirPath1, dirPath2, dirPath3, dirPath4, dirPath5]
+filteredRawDatas = [
+    filteredRawData1,
+    filteredRawData2,
+    filteredRawData3,
+    filteredRawData4,
+    filteredRawData5,
+]
+allEventsFiles = [
+    allEventsFile1,
+    allEventsFile2,
+    allEventsFile4,
+    allEventsFile4,
+    allEventsFile5,
+]
+for p, f, e in zip(paths, filteredRawDatas):
+    df = pd.read_csv(os.path.join(p, f))
+    output_file = os.path.join(p, "fitData.csv")
+    fit_data = pwAnalysis(df)
+    save_fit_data(fit_data,output_file)
+    # Process data and save to CSV
 # Example usage
 # df = pd.read_csv('your_data.csv')
-# fit_data = pwAnalysis(df)
 # Save fit_data to a file or database as needed
