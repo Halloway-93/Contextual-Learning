2ab0e988a61b77aa9d3ded547a9a34456d976a6b
Halloway-93 <hamzaelhallaoui@gmail.com>
2024-10-30 17:13:32 +0100

    pwanalysis parameters

--- directionCue/pwAnalysis.py
+++ directionCue/pwAnalysis.py
@@ -5,27 +5,22 @@ import piecewise_regression as pw
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import filtfilt, butter
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 
 
 # %%
-def pwAnalysis(csvPath):
-    df = pd.read_csv(csvPath)
+def pwAnalysis(df):
     # Getting the region of interest
-    fixOff = -200
-    latency = 120
-    df = df[(df.time >= fixOff + 20) & (df.time <= latency)]
     # Test on one subject and one condition and one trial
     # List of slopes
     # starting with 2 breakpoints
-    maxBreakPoints = 2
+    maxBreakPoints = 3
     slopes = [f"alpha{i+1}" for i in range(maxBreakPoints + 1)]
+    breakpoints = [f"breakpoint{i+1}" for i in range(maxBreakPoints)]
 
-    lastTrial = df[(df["trial"] > 150)]
+    df = df[(df["trial"] > 150)]
     allFit = []
-    for sub in lastTrial["sub"].unique():
-        df_sub = lastTrial[lastTrial["sub"] == sub]
+    for sub in df["sub"].unique():
+        df_sub = df[df["sub"] == sub]
         allFitConditions = []
         for p in df_sub.proba.unique():
             df_sub_p = df_sub[df_sub["proba"] == p]
@@ -33,9 +28,9 @@ def pwAnalysis(csvPath):
             allFitTrials = []
             for t in df_sub_p.trial.unique():
                 x = df_sub_p[df_sub_p["trial"] == t].time.values
-                y = df_sub_p[df_sub_p["trial"] == t].xp.values
+                y = interpolateData(df_sub_p[df_sub_p["trial"] == t].filtPos.values)
                 # Fitting the piecewise regression
-                pw_fit = pw.Fit(x, y, n_breakpoints=2)
+                pw_fit = pw.Fit(x, y, n_breakpoints=3)
                 # Geeting the slopes estimates
 
                 pw_results = pw_fit.get_results()
@@ -46,13 +41,19 @@ def pwAnalysis(csvPath):
                 # storing the alphas for each fitTrial
                 else:
                     alphas = []
-
+                    bps = []
                     for s in slopes:
                         if s in pw_estimates.keys():
                             alpha = pw_estimates[s]["estimate"]
                             alphas.append(alpha)
                             print(f"{s}:", alpha)
-                    allFitTrials.append(alphas)
+                    for b in breakpoints:
+                        if b in pw_estimates.keys():
+                            bp = pw_estimates[s]["estimate"]
+                            bps.append(bp)
+                            print(f"{b}:", bp)
+
+                    allFitTrials.append((alphas, bps))
             allFitConditions.append(allFitTrials)
         allFit.append(allFitConditions)
 
@@ -179,16 +180,23 @@ example["filtVelo2"] = interpolateData(process_eye_movement(example.filtPos.valu
 example
 # %%
 x = example.time.values
-y = interpolateData(example.velo.values)
+y = interpolateData(example.filtPos.values)
 # %%
 len(y)
 # %%
 plt.plot(x, y)
-# plt.plot(x, example.filtVelo.values)
+plt.plot(x, example.xp.values)
+plt.show()
+# %%
+
+plt.plot(x, example.filtVeloFilt.values)
 plt.show()
 # %%
 pw_fit = pw.Fit(x, y, n_breakpoints=3)
 print(pw_fit.summary())
+# %%
+pw_results = pw_fit.get_results()
+pw_results["estimates"]
 # %%
 # Plot the data, fit, breakpoints and confidence intervals
 pw_fit.plot_data(color="grey", s=20)
@@ -203,134 +211,11 @@ plt.close()
 # %%
 
 
-def piecewise_linear_regression_with_saccades(time, velocity):
-    """
-    Perform piecewise linear regression on eye movement velocity data,
-    including saccades.
-
-    Parameters:
-    -----------
-    time : numpy array
-        Time values for the velocity data
-    velocity : numpy array
-        Velocity data
-
-    Returns:
-    --------
-    piecewise_model : dict
-        Dictionary containing the piecewise linear regression model parameters
-    """
-    piecewise_model = {"breakpoints": [], "slopes": [], "intercepts": []}
-
-    # Start with a single linear regression
-    model = LinearRegression()
-    model.fit(time.reshape(-1, 1), velocity)
-    piecewise_model["breakpoints"] = []
-    piecewise_model["slopes"] = [model.coef_[0]]
-    piecewise_model["intercepts"] = [model.intercept_]
-
-    # Iteratively add breakpoints
-    prev_mse = mean_squared_error(velocity, model.predict(time.reshape(-1, 1)))
-    while len(piecewise_model["breakpoints"]) < 3:
-        best_breakpoint = None
-        best_mse = prev_mse
-
-        for i in range(1, len(time) - 1):
-            # Try inserting a breakpoint at each sample
-            left_model = LinearRegression()
-            right_model = LinearRegression()
-
-            left_mask = time < time[i]
-            right_mask = time >= time[i]
-
-            left_model.fit(time[left_mask].reshape(-1, 1), velocity[left_mask])
-            right_model.fit(time[right_mask].reshape(-1, 1), velocity[right_mask])
-
-            left_predict = left_model.predict(time[left_mask].reshape(-1, 1))
-            right_predict = right_model.predict(time[right_mask].reshape(-1, 1))
-            predict = np.concatenate([left_predict, right_predict])
-
-            new_mse = mean_squared_error(velocity, predict)
-            if new_mse < best_mse:
-                best_breakpoint = time[i]
-                best_mse = new_mse
-
-        if best_breakpoint is not None:
-            piecewise_model["breakpoints"].append(best_breakpoint)
-
-            left_model = LinearRegression()
-            right_model = LinearRegression()
-
-            left_mask = time < best_breakpoint
-            right_mask = time >= best_breakpoint
-
-            left_model.fit(time[left_mask].reshape(-1, 1), velocity[left_mask])
-            right_model.fit(time[right_mask].reshape(-1, 1), velocity[right_mask])
-
-            piecewise_model["slopes"].append(left_model.coef_[0])
-            piecewise_model["slopes"].append(right_model.coef_[0])
-            piecewise_model["intercepts"].append(left_model.intercept_)
-            piecewise_model["intercepts"].append(right_model.intercept_)
-        else:
-            break
-
-    return piecewise_model
-
-
-def plot_piecewise_fit(time, velocity, piecewise_model):
-    """
-    Plot the piecewise linear regression fit on the original velocity data.
-
-    Parameters:
-    -----------
-    time : numpy array
-        Time values for the velocity data
-    velocity : numpy array
-        Velocity data
-    piecewise_model : dict
-        Dictionary containing the piecewise linear regression model parameters
-    """
-    plt.figure(figsize=(12, 6))
-    plt.plot(time, velocity, label="Original Data")
-
-    # Plot the piecewise linear fit
-    fitted_velocity = []
-    for i in range(len(piecewise_model["breakpoints"]) + 1):
-        if i == 0:
-            start = 0
-            end = piecewise_model["breakpoints"][i]
-        elif i == len(piecewise_model["breakpoints"]):
-            start = piecewise_model["breakpoints"][-1]
-            end = time[-1]
-        else:
-            start = piecewise_model["breakpoints"][i - 1]
-            end = piecewise_model["breakpoints"][i]
-
-        mask = (time >= start) & (time < end)
-        segment_time = time[mask]
-        segment_velocity = (
-            piecewise_model["slopes"][i] * segment_time
-            + piecewise_model["intercepts"][i]
-        )
-        fitted_velocity.extend(segment_velocity)
-
-    plt.plot(time, fitted_velocity, label="Piecewise Linear Fit")
-    plt.xlabel("Time")
-    plt.ylabel("Velocity")
-    plt.title("Piecewise Linear Regression")
-    plt.legend()
-    plt.show()
-
-
 # %%
 x = example.time.values
 y = example.rawVelo.values
 # %%
 len(x)
-# %%
-model = piecewise_linear_regression(x, y)
-# %%
-plot_piecewise_fit(x, y, model)
 # %%
 
 
@@ -378,4 +263,5 @@ res = my_pwlf.fit(4)
 # predict for the determined points
 xHat = np.linspace(min(x), max(x), num=10000)
 yHat = my_pwlf.predict(xHat)
-
+# %%
+pwAnalysis(example)
