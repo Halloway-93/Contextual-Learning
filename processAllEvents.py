6a4cf5fc2e3baba9cb095f210162667b50d10ca2
Halloway-93 <hamzaelhallaoui@gmail.com>
2024-11-04 16:18:43 +0100

    corrected the function to handle only the ones who did not the events training

--- processAllEvents.py
+++ processAllEvents.py
@@ -36,10 +36,7 @@ def process_all_events(data_dir, filename="allEvents.csv"):
         if filepath.is_file():
             if filepath.suffix == ".csv" and filepath.stem.startswith("sub"):
                 df = read_file(filepath)
-                if df.sub==1 and df.proba==0.25:
-
-                    df["trial"] = [i + 11 for i in range(len(df))]
-                else:
+                if 'trial' not in df.columns: 
                     df["trial"] = [i + 1 for i in range(len(df))]
                 all_events.append(df)
             elif filepath.suffix == ".tsv":
